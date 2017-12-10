99d7d1b79a75f6f83eaa9f8797255a3598ddd56e
Lee Ji Hoon <thtjwls@gmail.com>
2017-12-10 19:38:15 +0900

    포트 잘못 설정해서 커밋함. 다시 수정

--- src/app/service/socket-connect.service.ts
+++ src/app/service/socket-connect.service.ts
@@ -4,8 +4,8 @@ import * as SocketIo from 'socket.io-client';
 @Injectable()
 export class SocketConnectService {
   io: any;
-  //WEB_SOCKET_URL = 'http://115.71.233.53:5000';
-  WEB_SOCKET_URL = 'http://127.0.0.1:5000';
+  WEB_SOCKET_URL = 'http://115.71.233.53:5000';
+  //WEB_SOCKET_URL = 'http://127.0.0.1:5000';
 
   constructor() {
     this.io = SocketIo(this.WEB_SOCKET_URL);
